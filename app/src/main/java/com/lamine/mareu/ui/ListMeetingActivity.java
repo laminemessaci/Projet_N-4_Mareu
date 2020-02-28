46623c2a02080125404e1a05b9a94194a2645d3b
laminemessaci <lamine.messaci18@gmail.com>
2020-02-28 12:26:24 +0100

    service -----> MeetingApieService

---
+++ app/src/main/java/com/lamine/mareu/ui/ListMeetingActivity.java
@@ -0,0 +1,28 @@
+package com.lamine.mareu.ui;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.lamine.mareu.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ListMeetingActivity extends AppCompatActivity {
+
+
+    @BindView (R.id.list) RecyclerView mRecyclerView;
+    @BindView (R.id.add) FloatingActionButton mFloatingActionButton;
+
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_list_meeting);
+        ButterKnife.bind (this);
+
+    }
+}
